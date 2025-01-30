63edf2016ddd2d30547008e4118fdc5c77559f14
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2025-01-30 10:50:31 -0300

    fix: na view de carga didatica por departamento não apareciam as disciplinas que ainda não tem docente

--- resources/views/relatorios/cargaDidatica/disciplinas/index.blade.php
+++ resources/views/relatorios/cargaDidatica/disciplinas/index.blade.php
@@ -35,7 +35,7 @@
                     </thead>
                     <tbody>
                         @foreach($turmas as $turma)
-                            @foreach($turma->docentes as $docente)
+                            @forelse($turma->docentes as $docente)
                                 <tr>
                                     <td class="text-center" style="vertical-align: middle;">{{ $docente->codpes }}</td>
                                     <td class="text-center" style="vertical-align: middle;">{{ $docente->nompes }}</td>
@@ -101,7 +101,73 @@
                                     @endif
                                     </td>
                                 </tr>
-                            @endforeach
+                            @empty
+                                <tr>
+                                    <td class="text-center" style="vertical-align: middle;">NA</td>
+                                    <td class="text-center" style="vertical-align: middle;">NA</td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->coddis }}</td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->nomdis }}</td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->codtur }}</td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->semestre->ano }}</td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->semestre->periodo }}</td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->creaul }}</td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->cretrb }}</td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->nummtr }}</td>
+                                    <td class="text-center" style="vertical-align: middle;white-space: nowrap;">
+                                        @foreach($turma->horarios->sortBy([
+                                            function ($a, $b) {
+                                                $dias = ["seg"=>1,"ter"=>2,"qua"=>3,"qui"=>4,"sex"=>5,"sab"=>6];
+                                                if($a["diasmnocp"] == $b["diasmnocp"]){
+                                                    return $a["horent"] <=> $b["horent"];
+                                                }
+                                                return $dias[$a["diasmnocp"]] <=> $dias[$b["diasmnocp"]];
+                                            }
+                                        ]) as $horario)
+                                            {{ $horario["diasmnocp"]." ".$horario["horent"]." ".$horario["horsai"] }}<br>
+                                        @endforeach
+                                    </td>
+                                    <td class="text-center" style="vertical-align: middle;">{{ $turma->nivel }}</td>
+                                    <?php
+                                        $query = App\Models\Turma::where("coddis",$turma->coddis)
+                                                    ->where("id", "!=", $turma->id)
+                                                    ->whereHas("semestre", function($query)use($turma){
+                                                        $query->where("ano", "<",$turma->semestre->ano);
+                                                        if($turma->semestre->periodo == 2){
+                                                            $query->orWhere(function($query2)use($turma){
+                                                                $query2->where("ano",$turma->semestre->ano)
+                                                                        ->where("periodo",1);
+                                                            });
+                                                        }
+                                                    });
+                                        if($turma->nivel == "Graduação"){
+                                            $query = $query->where("codtur", "like", "%".substr($turma->codtur,4,3));
+                                        }
+
+                                        $ts = $query->get()->sortByDesc("codtur")->slice(0,5);
+                                    ?>
+                                    <td class="text-center" style="vertical-align: middle;">
+                                        @if(count($ts) > 0)
+                                            @foreach($ts as $t)
+                                                {{ $t->semestre->periodo."/".$t->semestre->ano.": ".$t->nummtr }}<br>
+                                            @endforeach
+                                        @else
+                                            Indisponível
+                                        @endif
+                                    </td>
+                                    <td class="text-center" style="vertical-align: middle;">
+                                    @if($turma->dobradinha()->exists())
+                                        Provável dobradinha com 
+                                        @foreach($turma->dobradinha->turmas()->where("id","!=",$turma->id)->get() as $t)
+                                            @if($t->nivel == "Graduação")
+                                                {{ $t->coddis."(".$t->codtur.") "}}
+                                            @elseif($t->nivel == "Pós Graduação")
+                                                {{ $t->coddis." "}}
+                                            @endif
+                                        @endforeach
+                                    @endif
+                                    </td>
+                                </tr>
+                            @endforelse
                         @endforeach
                     </tbody>
                 </table>
